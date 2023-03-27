aeb6b3bfdc1e68ddcf84625575cf3491e2f1003b
Marcio <marcioxerfan@gmail.com>
2023-03-27 12:07:01 -0300

    Final Commit 2

---
+++ js/script-tela-cadastro-produto.js
@@ -0,0 +1,12 @@
+let btnAdd = document.querySelector("#addBotao");
+
+/* criar um evento de clique para o botão que irá executar a function */
+btnAdd.addEventListener("click", function(){
+    let inputValor = document.querySelector("#meuInput").value;
+    if(inputValor === ""){
+        alert("Você não preencheu os campos!!")
+    }else{
+        alert("Cadastro do produto realizado com sucesso!")
+    }
+    document.querySelector('#meuInput').value = "";
+});
